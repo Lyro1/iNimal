b0c2e0d0fa8daef880b8d6829a61620b0ad033fe
Louis Dupont <contact@lyro.fr>
2022-05-18 21:13:49 +0200

    Initial Commit

---
+++ Shared/iNimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNimalApp.swift
+//  Shared
+//
+//  Created by Louis Dupont on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iNimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
