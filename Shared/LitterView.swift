2d73c37b53599479656c197c2130ca0b20e0b755
Louis Dupont <contact@lyro.fr>
2022-12-19 21:33:50 +0100

    Tried to build a swipe action... But failed

--- Shared/LitterView.swift
+++ Shared/LitterView.swift
@@ -13,44 +13,55 @@ struct LitterView: View {
     var animationDuration = 0.2
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                ZStack {
-                    Circle()
-                        .strokeBorder(.gray.opacity(0.1), lineWidth: 1)
-                        .background(Circle().fill(.white))
-                    Text(litter.emoji)
-                        .font(.largeTitle)
+        ScrollView(.horizontal, showsIndicators: false) {
+            GeometryReader { geometry in
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .strokeBorder(.gray.opacity(0.1), lineWidth: 1)
+                                    .background(Circle().fill(.white))
+                                Text(litter.emoji)
+                                    .font(.title2)
+                            }
+                            .frame(width: 48, height: 48)
+                            .position(x: 54, y: 48)
+                            .shadow(color: .gray.opacity(0.1), radius: 4)
+                        }
+                        .frame(width: geometry.size.width, height: 100)
+                        .background(Color("Pastel Green"))
+                        HStack {
+                            Text(litter.name)
+                                .font(.headline)
+                            Spacer()
+                            Text(litter.getTimeRemaining())
+                                .font(.largeTitle)
+                                .bold()
+                        }
+                        .padding([.leading, .trailing], 36)
+                        .padding([.top, .bottom], 24)
+                    }
+                    .frame(width: geometry.size.width)
                 }
-                .frame(width: 48, height: 48)
-                .position(x: 54, y: 48)
-                .shadow(color: .gray.opacity(0.1), radius: 4)
-                
-                Spacer()
+                .frame(height: 100)
             }
-            .background(Color("Pastel Green"))
-            HStack {
-                Text(litter.name)
-                    .font(.headline)
-                Spacer()
-                Text(litter.getTimeRemaining())
-                    .font(.largeTitle)
-                    .bold()
-            }
-            .padding([.leading, .trailing], 36)
-            .padding([.top, .bottom], 24)
+            .frame(height: 100)
         }
         .border(.gray.opacity(0.1))
         .background(Color.white)
         .cornerRadius(20)
         .shadow(color: .gray.opacity(0.1), radius: 4)
         .scaleEffect(tap ? 0.98 : 1)
-        .onTapGesture {
-            tap.toggle()
-            DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration) {
-                tap.toggle()
-            }
-        }
+        .simultaneousGesture(
+            DragGesture(minimumDistance: 0)
+                .onChanged({ gesture in
+                    tap = true;
+                })
+                .onEnded({ gesture in
+                    tap = false;
+                })
+        )
         .animation(.spring(response: animationDuration, dampingFraction: 0.4), value: tap)
     }
 }
